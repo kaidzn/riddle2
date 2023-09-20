f266bd20d7f8df4fae3582eeb7e502e3ba343391
kaidzn <kaidzn17@gmail.com>
2023-09-21 00:55:55 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito', 'sans'], 
+        'comfortaa': ['Comfortaa', 'sans'],
+      },
+    },
+  },
+  plugins: [],
+}
+
